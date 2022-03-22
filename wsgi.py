c818e4accf712defd57d518aa2ba077b6997df3b
King-Del-ErnestO <delernestoa@gmail.com>
2022-03-22 10:54:25 +0100

    initial commit

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
-from portapp import app
+from portapp.portfolio import app
 import os
 
-prot = int(os.environ.get("PORT", 5000))
+port = int(os.environ.get("PORT", 5000))
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=port, debug=True)
