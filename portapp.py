7cd0f1a292f69c104cc1688d3b815b6dde2c6c99
King-Del-ErnestO <delernestoa@gmail.com>
2022-03-26 18:54:53 +0100

    initial commit

--- portapp.py
+++ portapp.py
@@ -3,9 +3,9 @@ from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('del-port.html')
 
 
 if __name__ == '__main__':
